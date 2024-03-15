d71ca81d5154c9a6f4bfc7f9bac5a82479131c60
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2024-03-14 20:26:57 -0500

    better layout

--- src/Gui/ContainerMenu/main.js
+++ src/Gui/ContainerMenu/main.js
@@ -1,1 +1,16 @@
-import { template } from '../utils/shortcut'
+import { template } from '../../utils/shortcut'
+import Settings from '../Menus/Settings'
+import Console from '../Menus/Console'
+import Games from '../Menus/Games'
+import Shortcuts from '../Menus/Shortcuts'
+
+const DefaultMenus = {
+    Shortcuts,
+    Games,
+    Console,
+    Settings,
+}
+
+export default MainMenu {
+    
+}
--- src/Gui/Menus/Shortcut-Maker/shortcut-maker.js
+++
@@ -1,3 +0,0 @@
-import { template } from '../utils/shortcut'
-
-
