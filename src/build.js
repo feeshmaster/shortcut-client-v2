c5436f0a36754e6993160db312afa6eb2744c8ec
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2024-03-17 18:42:18 -0500

    added more things to work on again :/

--- src/build.js
+++ src/build.js
@@ -1,20 +1,26 @@
+//imports
+import ContainerMenu from './Gui/ContainerMenu/Menu'
+import EntryBtn from './Gui/EntryButton/EntryButton'
+import MenuInits from './Gui/Menu_inits'
 
 export default  {
     init() {
-       
+       this.root = document.body || document
        //initiate each part in order
        this.init_ContainerMenu()
        this.init_Menus()
        this.init_EntryButton()
     },
     init_ContainerMenu() {
-
+       ContainerMenu.init(this.root)
     },
     init_Menus() {
-
+       for (Menu in MenuInits) {
+          ContainerMenu.add(Menu.init())
+       }
     },
     init_EntryButton() {
-
+       EntryBtn.init(root)
     }
 
 }
