d3982866a219daf845548b678322cd76b294f104
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2024-03-29 20:21:13 -0500

    added drag ;)
    im so good right?

--- src/Gui/ContainerMenu/Menu.js
+++ src/Gui/ContainerMenu/Menu.js
@@ -12,6 +12,7 @@ export default class ContainerMenu {
       this.buildClient()
       this.sideBarOptions = {}
       this.currentMenu
+      this.addDrag()
     }
     show() {
        this.container.style.opacity = 1;
@@ -61,6 +62,62 @@ export default class ContainerMenu {
     }
     unbindEvent(element, event, handler) {
       element.addEventListener(event, handler)
+    }
+
+    addDrag() {
+      this.container.style.position = "absolute"
+      this.isDrag = false, this.offsetX, this.offsetY;
+      this.bindEvent(this.topbar, "mousedown", this.dragStartHandler, this)
+      this.bindEvent(this.topbar, "touchstart", this.dragStartHandler, this, { passive: true })
+      this.bindEvent(window, "mouseup", this.mouseUpHandler, this)
+      this.bindEvent(window, "touchend", this.mouseUpHandler, this)
+      this.bindEvent(window, "resize", this.adjustToFit, this)
+    }
+    adjustToFit() {
+      this.oldLeft = this.container.style.left.slice(0, -2)
+      this.oldTop = this.container.style.top.slice(0, -2)
+      if (this.oldLeft > window.innerWidth - this.container.offsetWidth) {
+        this.oldLeft = window.innerWidth - this.container.offsetWidth
+      }
+      if (this.oldTop > window.innerHeight - this.container.offsetHeight) {
+        this.oldTop = window.innerHeight - this.container.offsetHeight
+      }
+      this.container.style.left = this.oldLeft + "px"
+      this.container.style.top = this.oldTop + "px"
+    }
+    dragStartHandler(e) {
+      this.bindEvent(document, "pointermove", this.mouseMoveHandler, this)
+      this.bindEvent(document, "touchmove", this.mouseMoveHandler, this)
+      this.isDrag = true;
+      this.offsetX = e.type === 'touchstart' ? e.touches[0].clientX - this.container.getBoundingClientRect().left : e.clientX - this.container.getBoundingClientRect().left;
+      this.offsetY = e.type === 'touchstart' ? e.touches[0].clientY - this.container.getBoundingClientRect().top : e.clientY - this.container.getBoundingClientRect().top;
+    }
+    mouseUpHandler(e) {
+      this.unbindEvent(document, "pointermove", this.mouseMoveHandler)
+      this.unbindEvent(document, "touchmove", this.mouseMoveHandler)
+      this.isDrag = false;
+    }
+    mouseMoveHandler(e) {
+      if (this.isDrag) {
+        e.preventDefault();
+        this.Left = (e.type === 'touchmove' ? e.touches[0].clientX : e.clientX) - this.offsetX
+        this.Top = (e.type === 'touchmove' ? e.touches[0].clientY : e.clientY) - this.offsetY
+  
+        if (this.Left < 0) {
+          this.Left = 0
+        }
+        if (this.Top < 0) {
+          this.Top = 0
+        }
+        if (this.Left > window.innerWidth - this.container.offsetWidth) {
+          this.Left = window.innerWidth - this.container.offsetWidth
+        }
+        if (this.Top > window.innerHeight - this.container.offsetHeight) {
+          this.Top = window.innerHeight - this.container.offsetHeight
+        }
+        this.container.style.top = this.Top + "px"
+        this.container.style.left = this.Left + "px"
+      }
     }
     
 }
